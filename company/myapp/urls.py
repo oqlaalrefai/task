5e70dd7cedb9c2f30d4ac79a80dc8bf608c6f3a2
oqlaalrefai <72412478+oqlaalrefai@users.noreply.github.com>
2022-10-09 17:05:22 +0300

    Update urls.py

--- company/myapp/urls.py
+++ company/myapp/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from django.conf.urls import url
 from . import views
 urlpatterns = [
+    path('', views.home, name="home"),
     path('employee',views.all_employees, name='list-employees'),
     path('department', views.all_departments, name='list-departments'),
-    # url(r'^dep/(?P<dep_id>[0-9]+)',views.all_departments, name='list-departments'),
     path('addemp', views.add_emp, name='add-emp'),
     path('adddep', views.add_dep, name='add-dep'),
     path('department/<depid>', views.show_dep, name='show-dep'),
